e4cdc482435d77e631d58fe4dad7738ac685fb66
OutlawDisco <aoutlaw00@gmail.com>
2023-07-09 10:26:02 -0600

    moved quiz , updated functions to include timer

--- assets/script.js
+++ assets/script.js
@@ -1,93 +1,155 @@
 // Variables
-var infoBox = document.querySelector(".start-box");
+const startBtn = document.querySelector(".start_btn button");
+const infoBox = document.querySelector(".info_box");
+const exitBtn = infoBox.querySelector(".buttons .quit");
+const continueBtn = infoBox.querySelector(".buttons .restart");
+const quizBox = document.querySelector(".quiz_box");
+const optionList = document.querySelector(".option_list");
+const timeCount = quizBox.querySelector(".timer .timer_sec");
 
-var quizEl = document.querySelector(".quiz-box");
+//If start button is clicked
+startBtn.onclick = () => {
+  infoBox.classList.add("activeInfo"); //show info box
+};
 
-var startBtn = document.getElementById("start-btn");
+//If No thanks button is clicked
+exitBtn.onclick = () => {
+  infoBox.classList.remove("activeInfo"); //hide info box
+};
 
-var questions = [
-  {
-    question: "What are humans?",
-    answers: [
-      "muggles",
-      "silly creatures from outerspace ",
-      "cbone-bags",
-      "normies",
-    ],
-    correct: [1],
-  },
-  {
-    question: "Who is not a fraggle?",
-    answers: ["Gobo", "Mokey", "Wembley", "Gorg"],
-    correct: [3],
-  },
-  {
-    question: "What is a fraggles favorite snack?",
-    answers: ["apples", "carrots", "doozer buildings", "socks"],
-    correct: [2],
-  },
-  {
-    question: "Every Fraggle has a job, what is Boomers?",
-    answers: ["laundry", "picking radished", "depression", "singing"],
-    correct: [0],
-  },
-  {
-    question: "Who is Marjory?",
-    answers: [
-      "a heap of trash",
-      "a wise voice of reason",
-      "a great singer",
-      "all of the above",
-    ],
-    correct: [3],
-  },
-  {
-    question: "Uncle Traveling Matt sends postcards to who?",
-    answers: ["Red", "Mokey", "Gobo", "Wembley"],
-    correct: [2],
-  },
-  {
-    question: "Who is the best swimmer?",
-    answers: ["Red", "Gobo", "Wembley", "Boomer"],
-    correct: [0],
-  },
-  {
-    question: "Where do fraggles live?",
-    answers: ["Detroit", "Fraggle Rock", "Boston", "LA"],
-    correct: [1],
-  },
-];
-var questionEl = document.getElementById("question");
-var answerBtn = document.getElementById("answer-buttons");
+//If lets-go button is clicked
+continueBtn.onclick = () => {
+  infoBox.classList.remove("activeInfo"); //hide info box
+  quizBox.classList.add("activeQuiz"); //Show Quiz Box
+  showQuestions(0);
+  queCounter(1);
+  startTimer(30);
+};
 
-let currentQuestIndex = 0;
-let score = 0;
+let queCount = 0;
+let queNumb = 1;
+let counter;
+let timerValue = 30;
+let userScore = 0;
 
-function startQuiz() {
-  currentQuestIndex = 0;
-  score = 0;
-  showQuestion();
+const nextBtn = quizBox.querySelector(".next_btn");
+const resultBox = document.querySelector(".result_box");
+const reQuiz = resultBox.querySelector(".buttons .restart");
+const quitQuiz = resultBox.querySelector(".buttons .quit");
+
+nextBtn.onclick = () => {
+  if (queCount < questions.length - 1) {
+    queCount++;
+    queNumb++;
+    showQuestions(queCount);
+    queCounter(queNumb);
+    nextBtn.style.display = "none";
+  } else {
+    console.log("questions completed");
+    showResults();
+  }
+};
+//get questions and options from array
+function showQuestions(index) {
+  const queText = document.querySelector(".question_text");
+  let queTag =
+    "<span>" +
+    questions[index].number +
+    ". " +
+    questions[index].question +
+    "</span>";
+  let optionTag =
+    '<div class="option">' +
+    questions[index].options[0] +
+    "<span></span></div>" +
+    '<div class="option">' +
+    questions[index].options[1] +
+    "<span></span></div>" +
+    '<div class="option">' +
+    questions[index].options[2] +
+    "<span></span></div>" +
+    '<div class="option">' +
+    questions[index].options[3] +
+    "<span></span></div>";
+  queText.innerHTML = queTag;
+  optionList.innerHTML = optionTag;
+
+  const option = optionList.querySelectorAll(".option");
+  for (let i = 0; i < option.length; i++) {
+    option[i].setAttribute("onclick", "optionSelected(this)");
+  }
 }
 
-function showQuestion() {
-  resetState();
-  let currentQuest = questions[currentQuestIndex];
-  let questNum = (currentQuestIndex = 1);
-  questionEl.innerHTML = questNum + ". " + currentQuest.question;
+function optionSelected(answer) {
+  let userAnswer = answer.textContent;
+  let correctAnswer = questions[queCount].answer;
+  let allOptions = optionList.children.length;
 
-  currentQuest.answers.forEach((answer) => {
-    var button = document.createElement("button");
-    button.innerHTML = answer.text;
-    button.classList.add("btn");
-    answerBtn.appendChild(button);
-  });
+  if (userAnswer == correctAnswer) {
+    userScore += 1;
+    answer.classList.add("correct");
+    console.log("Answer is correct");
+  } else {
+    answer.classList.add("wrong");
+    console.log("wrong!");
+
+    for (let i = 0; i < allOptions; i++) {
+      if (optionList.children[i].textContent == correctAnswer) {
+        optionList.children[i].setAttribute("class", "option correct");
+      }
+    }
+  }
+  //once answer is selected disable all options
+  for (let i = 0; i < allOptions; i++) {
+    optionList.children[i].classList.add("disabled");
+  }
+  nextBtn.style.display = "block";
 }
-startBtn.addEventListener("click", function () {
-  infoBox.classList.add("hide");
-  //execute timer function
 
-  startQuiz();
-});
-function resetState() {
-  while (answerBtn.firstChild) answerBtn.removeChild(answer.firstChild);
+function queCounter(index) {
+  const btmCounter = quizBox.querySelector(".total_questions");
+  let totalQuesCountTag =
+    "<span><p>" + index + "</p>of<p>" + questions.length + "</p></span>";
+  btmCounter.innerHTML = totalQuesCountTag;
+}
+
+function showResults() {
+  infoBox.classList.remove("activeInfo"); //hide info box
+  quizBox.classList.remove("activeQuiz"); //hide Quiz Box
+  resultBox.classList.add("activeResult"); //Show results Box
+  const scoreText = resultBox.querySelector(".score_text");
+  if (userScore > 3) {
+    let scoreTag =
+      "<span>You got <p>" +
+      userScore +
+      "</p> out of <p>" +
+      questions.length +
+      "</p> correct!</span>";
+    scoreTag.innerHTML = scoreTag;
+  } else if (userScore > 1){
+    let scoreTag = "<span>You got <p>" +
+    userScore +
+    "</p> out of <p>" +
+    questions.length +
+    "</p> correct!</span>";
+  scoreTag.innerHTML = scoreTag;
+  } else(userScore > 1){
+    let scoreTag = "<span>You got only <p>" +
+    userScore +
+    "</p> out of <p>" +
+    questions.length +
+    "</p> correct!</span>";
+  scoreTag.innerHTML = scoreTag;
+}
+
+function startTimer(time) {
+  counter = setInterval(timer, 1000);
+  function timer() {
+    timeCount.textContent = time;
+    time--;
+    if (time < 0) {
+      clearInterval(counter);
+      timeCount.textContent = "00";
+    };
+  }
 }
