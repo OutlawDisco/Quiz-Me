c15e9f68b8f966faf49062cb48dc9c71cc8cd6c4
OutlawDisco <aoutlaw00@gmail.com>
2023-07-09 10:23:50 -0600

    created questions.js for quiz

---
+++ assets/questions.js
@@ -0,0 +1,61 @@
+let questions = [
+  {
+    number: 1,
+    question: "What are humans?",
+
+    options: [
+      "muggles",
+      "silly creatures from outerspace ",
+      "bone-bags",
+      "normies",
+    ],
+    answer: "silly creatures from outerspace ",
+  },
+  {
+    number: 2,
+    question: "Who is not a fraggle?",
+    options: ["Gobo", "Mokey", "Wembley", "Gorg"],
+    answer: "Gorg",
+  },
+  {
+    number: 3,
+    question: "What is a fraggles favorite snack?",
+    options: ["apples", "carrots", "doozer buildings", "socks"],
+    answer: "doozer buildings",
+  },
+  {
+    number: 4,
+    question: "Every Fraggle has a job, what is Boomers?",
+    options: ["laundry", "picking radished", "depression", "singing"],
+    answer: "laundry",
+  },
+  {
+    number: 5,
+    question: "Who is Marjory?",
+    options: [
+      "a heap of trash",
+      "a wise voice of reason",
+      "a great singer",
+      "all of the above",
+    ],
+    answer: "all of the above",
+  },
+  {
+    number: 6,
+    question: "Uncle Traveling Matt sends postcards to who?",
+    options: ["Red", "Mokey", "Gobo", "Wembley"],
+    answer: "Gobo",
+  },
+  {
+    number: 7,
+    question: "Who is the best swimmer?",
+    options: ["Red", "Gobo", "Wembley", "Boomer"],
+    answer: "Red",
+  },
+  {
+    number: 8,
+    question: "Where do fraggles live?",
+    options: ["Detroit", "Fraggle Rock", "Boston", "LA"],
+    answer: "Fraggle Rock",
+  },
+];
